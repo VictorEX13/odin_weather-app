d99297ab9ea5586a626e0b7ad1f82c12226fba39
Victor <victorshiira4869@gmail.com>
2023-03-15 21:24:35 -0300

    Set up npm and webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ title: "WWWeather" })],
+  mode: "development",
+  devtool: "inline-source-map",
+};
